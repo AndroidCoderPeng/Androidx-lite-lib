ca980630cb4453da57591a34f223c34130495b05
pengxianhong <290677893@qq.com>
2022-06-14 00:06:02 +0800

    feat(PageNavigationManager): 添加帮助类-8

---
+++ lite/src/main/java/com/pengxh/androidx/lite/utils/PageNavigationManager.java
@@ -0,0 +1,68 @@
+package com.pengxh.androidx.lite.utils;
+
+import android.app.Activity;
+
+import java.util.Stack;
+
+public class PageNavigationManager {
+    private static final Stack<Activity> activityStack = new Stack<>();
+
+    /**
+     * 添加Activity到堆栈
+     */
+    public static void addActivity(Activity activity) {
+        if (activity == null) {
+            return;
+        }
+        activityStack.push(activity);
+    }
+
+    /**
+     * 获取当前Activity（堆栈中最后一个压入的）
+     */
+    public static Activity currentActivity() {
+        return activityStack.lastElement();
+    }
+
+    /**
+     * 结束当前Activity（堆栈中最后一个压入的）
+     */
+    public static void finishCurrentActivity() {
+        Activity activity = activityStack.pop();
+        activity.finish();
+    }
+
+    /**
+     * 结束指定的Activity
+     */
+    public static void finishActivity(Activity activity) {
+        if (activity == null) {
+            return;
+        }
+        activityStack.remove(activity);
+        if (!activity.isFinishing()) {
+            activity.finish();
+        }
+    }
+
+    /**
+     * 结束指定类名的Activity
+     */
+    public static <T> void finishActivity(Class<T> clazz) {
+        for (Activity activity : activityStack) {
+            if (activity.getClass() == clazz) {
+                finishActivity(activity);
+            }
+        }
+    }
+
+    /**
+     * 结束所有Activity
+     */
+    public static void finishAllActivity() {
+        for (Activity activity : activityStack) {
+            activity.finish();
+        }
+        activityStack.clear();
+    }
+}
