277f46d967a2d5bb1cef46bed0ae268e83152d37
Pengxh <290677893@qq.com>
2022-06-14 09:08:45 +0800

    feat(TimeOrDateUtil): 添加帮助类-10

--- lite/src/main/java/com/pengxh/androidx/lite/utils/TimeOrDateUtil.java
+++ lite/src/main/java/com/pengxh/androidx/lite/utils/TimeOrDateUtil.java
@@ -1,12 +1,14 @@
 package com.pengxh.androidx.lite.utils;
 
+import android.text.TextUtils;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class TimeOrDateUtil {
-    private static final String TAG = "TimeOrDateUtil";
     private static SimpleDateFormat dateFormat;
 
     /**
@@ -69,5 +71,63 @@ public class TimeOrDateUtil {
     public static String millsToTime(long millSeconds) {
         dateFormat = new SimpleDateFormat("mm:ss", Locale.CHINA);
         return dateFormat.format(new Date(millSeconds));
+    }
+
+    /**
+     * 根据时间戳得到上个月的日期
+     */
+    public static String timestampToLastMonthDate(long timestamp) {
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date(timestamp));
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - 29);
+        return dateFormat.format(calendar.getTime());
+    }
+
+    /**
+     * 根据时间戳得到上周的日期
+     */
+    public static String timestampToLastWeekDate(long timestamp) {
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.CHINA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date(timestamp));
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - 6);
+        return dateFormat.format(calendar.getTime());
+    }
+
+    /**
+     * 根据时间戳得到上周的时间
+     */
+    public static String timestampToLastWeekTime(long timestamp) {
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date(timestamp));
+        calendar.set(Calendar.DAY_OF_YEAR, calendar.get(Calendar.DAY_OF_YEAR) - 6);
+        return dateFormat.format(calendar.getTime());
+    }
+
+    /**
+     * 获取当前月份所在季度
+     */
+    public static int obtainQuarterOfYear(long timestamp) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date(timestamp));
+        return calendar.get(Calendar.MONTH) / 3 + 1;
+    }
+
+    /**
+     * 判断时间是否早于当前时间
+     */
+    public static boolean isEarlierThanStart(long timestamp, String date) {
+        if (TextUtils.isEmpty(date)) {
+            return false;
+        }
+        dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA);
+        try {
+            return timestamp < dateFormat.parse(date).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
