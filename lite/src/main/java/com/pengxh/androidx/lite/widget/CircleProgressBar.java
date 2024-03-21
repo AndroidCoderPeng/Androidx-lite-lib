ff3119ef80f4a297fbf634f8415fa776a7b6efd2
Pengxh <290677893@qq.com>
2024-03-21 09:44:30 +0800

    refactor(*): 优化CircleProgressBar

--- lite/src/main/java/com/pengxh/androidx/lite/widget/CircleProgressBar.java
+++ lite/src/main/java/com/pengxh/androidx/lite/widget/CircleProgressBar.java
@@ -1,6 +1,5 @@
 package com.pengxh.androidx.lite.widget;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -8,39 +7,37 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.os.Handler;
-import android.os.Message;
 import android.text.TextPaint;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.pengxh.androidx.lite.R;
 import com.pengxh.androidx.lite.hub.FloatHub;
 import com.pengxh.androidx.lite.hub.IntHub;
-import com.pengxh.androidx.lite.utils.WeakReferenceHandler;
 
 /**
  * 圆形进度条
  */
-public class CircleProgressBar extends View implements Handler.Callback {
-
-    private final Context context;
-    private int ringRadius;
-    private RectF rectF;
+public class CircleProgressBar extends View {
+    private final int ringRadius;
+    private final RectF rectF;
 
     //控件边长
-    private int viewSideLength;
-    private int ringStroke;
-    private Rect viewRect;
+    private final int viewSideLength;
+    private final int ringStroke;
+    private final Rect viewRect;
     private Paint guidePaint;
 
-    private int backgroundColor;
-    private int foregroundColor;
-    private String text = "";
+    private final int backgroundColor;
+    private final int foregroundColor;
+    private final int textColor;
+    private final String text;
+    private final float textSize;
+    private final boolean hideText;
+
     private float centerX = 0f;
     private float centerY = 0f;
     private Paint backgroundPaint;
@@ -52,7 +49,6 @@ public class CircleProgressBar extends View implements Handler.Callback {
 
     //当前测量值转为弧度扫过的角度
     private float sweepAngle = 0f;
-    private WeakReferenceHandler weakReferenceHandler;
 
     public CircleProgressBar(Context context) {
         this(context, null);
@@ -64,37 +60,30 @@ public class CircleProgressBar extends View implements Handler.Callback {
 
     public CircleProgressBar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        this.context = context;
-        weakReferenceHandler = new WeakReferenceHandler(this);
-
         TypedArray type = context.obtainStyledAttributes(attrs, R.styleable.CircleProgressBar);
-        ringRadius = type.getDimensionPixelOffset(R.styleable.CircleProgressBar_cpb_ring_radius, IntHub.dp2px(context, 100));
+        ringRadius = type.getDimensionPixelSize(R.styleable.CircleProgressBar_cpb_ring_radius, IntHub.dp2px(context, 100));
         rectF = new RectF(-ringRadius, -ringRadius, ringRadius, ringRadius);
-        ringStroke = type.getDimensionPixelOffset(R.styleable.CircleProgressBar_cpb_ring_stroke, IntHub.dp2px(context, 10));
-        //需要给外围刻度留位置
-        viewSideLength = ringRadius + IntHub.dp2px(context, 30);
+        ringStroke = type.getDimensionPixelSize(R.styleable.CircleProgressBar_cpb_ring_stroke, IntHub.dp2px(context, 10));
+        viewSideLength = ringRadius + IntHub.dp2px(context, 10);
         //辅助框
         viewRect = new Rect(-viewSideLength, -viewSideLength, viewSideLength, viewSideLength);
 
-        backgroundColor = type.getColor(R.styleable.CircleProgressBar_cpb_backgroundColor, Color.LTGRAY);
-        foregroundColor = type.getColor(R.styleable.CircleProgressBar_cpb_foregroundColor, Color.BLUE);
+        backgroundColor = type.getColor(R.styleable.CircleProgressBar_cpb_background, Color.LTGRAY);
+        foregroundColor = type.getColor(R.styleable.CircleProgressBar_cpb_foreground, Color.BLUE);
         text = type.getString(R.styleable.CircleProgressBar_cpb_text);
-
+        textColor = type.getColor(R.styleable.CircleProgressBar_cpb_text_color, Color.DKGRAY);
+        textSize = type.getDimension(R.styleable.CircleProgressBar_cpb_text_size, 18f);
+        hideText = type.getBoolean(R.styleable.CircleProgressBar_cpb_hide_text, false);
         type.recycle();
         //初始化画笔
         initPaint();
-    }
-
-    @Override
-    public boolean handleMessage(@NonNull Message msg) {
-        return true;
     }
 
     private void initPaint() {
         guidePaint = new Paint();
         guidePaint.setColor(Color.LTGRAY);
         guidePaint.setStyle(Paint.Style.STROKE);
-        guidePaint.setStrokeWidth(FloatHub.dp2px(context, 1f));
+        guidePaint.setStrokeWidth(FloatHub.dp2px(getContext(), 1f));
         guidePaint.setAntiAlias(true);
 
         //背景色画笔
@@ -116,8 +105,8 @@ public class CircleProgressBar extends View implements Handler.Callback {
         //文字画笔
         textPaint = new TextPaint();
         textPaint.setTextAlign(Paint.Align.CENTER);
-        textPaint.setColor(Color.LTGRAY);
-        textPaint.setTextSize(FloatHub.dp2px(context, 14f));
+        textPaint.setColor(textColor);
+        textPaint.setTextSize(textSize);
         textPaint.setAntiAlias(true);
     }
 
@@ -159,7 +148,6 @@ public class CircleProgressBar extends View implements Handler.Callback {
         setMeasuredDimension(mWidth, mHeight);
     }
 
-    @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -167,26 +155,28 @@ public class CircleProgressBar extends View implements Handler.Callback {
          * 画布移到中心位置
          * */
         canvas.translate(centerX, centerY);
-        drawGuides(canvas);
+//        drawGuides(canvas);
 
         //绘制进度条背景
         canvas.drawCircle(0f, 0f, ringRadius, backgroundPaint);
 
-        //绘制上面百分比
-        Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
-        float top = fontMetrics.top;
-        float bottom = fontMetrics.bottom;
-        if (TextUtils.isEmpty(currentValue)) {
-            canvas.drawText("###", 0f, (top + bottom) / 2, textPaint);
-        } else {
-            canvas.drawText(currentValue, 0f, (top + bottom) / 2, textPaint);
-        }
+        if (!hideText) {
+            //绘制上面百分比
+            Paint.FontMetrics fontMetrics = textPaint.getFontMetrics();
+            float top = fontMetrics.top;
+            float bottom = fontMetrics.bottom;
+            if (TextUtils.isEmpty(currentValue)) {
+                canvas.drawText("###", 0f, (top + bottom) / 2, textPaint);
+            } else {
+                canvas.drawText(currentValue, 0f, (top + bottom) / 2, textPaint);
+            }
 
-        //绘制下面Tip文字
-        if (TextUtils.isEmpty(text)) {
-            canvas.drawText("###", 0f, -(top + bottom) * 1.5f, textPaint);
-        } else {
-            canvas.drawText(text, 0f, -(top + bottom) * 1.5f, textPaint);
+            //绘制下面Tip文字
+            if (TextUtils.isEmpty(text)) {
+                canvas.drawText("###", 0f, -(top + bottom) * 1.5f, textPaint);
+            } else {
+                canvas.drawText(text, 0f, -(top + bottom) * 1.5f, textPaint);
+            }
         }
 
         //绘制前景进度
@@ -216,36 +206,19 @@ public class CircleProgressBar extends View implements Handler.Callback {
             this.currentValue = value + "%";
         }
 
-        int i = 0;
-        while (i <= value) {
-            weakReferenceHandler.post(updateProgressRunnable.setProgress(i));
-            i++;
-        }
-//        new Thread(() -> {
-//            for (int i = 0; i < value; i++) {
-//                weakReferenceHandler.post(updateProgressRunnable.setProgress(i));
-//            }
-//        }).start();
+        final int[] i = {0};
+        post(new Runnable() {
+            @Override
+            public void run() {
+                i[0]++;
+                sweepAngle = (float) i[0] * 360 / 100;
+                invalidate();
+                if (i[0] <= value) {
+                    postDelayed(this, 10);
+                } else {
+                    removeCallbacks(this);
+                }
+            }
+        });
     }
-
-    private interface UpdateProgressRunnable extends Runnable {
-        UpdateProgressRunnable setProgress(int progress);
-    }
-
-    private final UpdateProgressRunnable updateProgressRunnable = new UpdateProgressRunnable() {
-
-        private int progress = 0;
-
-        @Override
-        public UpdateProgressRunnable setProgress(int progress) {
-            this.progress = progress;
-            return this;
-        }
-
-        @Override
-        public void run() {
-            sweepAngle = (float) progress * 360 / 100;
-            invalidate();
-        }
-    };
 }
