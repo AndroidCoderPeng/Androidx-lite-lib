367820ab052ea8b24d6d2f4758429188f911b33f
pengxianhong <290677893@qq.com>
2024-04-22 13:49:09 +0800

    refactor(*): 优化EditableImageAdapter

--- lite/src/main/java/com/pengxh/androidx/lite/adapter/EditableImageAdapter.java
+++ lite/src/main/java/com/pengxh/androidx/lite/adapter/EditableImageAdapter.java
@@ -13,8 +13,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.pengxh.androidx.lite.R;
-import com.pengxh.androidx.lite.hub.ContextHub;
-import com.pengxh.androidx.lite.hub.IntHub;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,23 +24,25 @@ public class EditableImageAdapter extends RecyclerView.Adapter<ViewHolder> {
 
     private static final String TAG = "EditableImageAdapter";
     private final Context context;
+    private final int viewWidth;
     private final int imageCountLimit;
-    private final int screenWidth;
-    private final int spacing;
+    private final int spanCount;
     private final LayoutInflater layoutInflater;
     private List<String> images = new ArrayList<>();
 
     /**
      * 数量可编辑图片适配器
      *
-     * @param imageCountLimit 最多显示几张图片，每行3张图片
-     * @param spacing         上下左右外边距，无需在 {@link androidx.recyclerview.widget.RecyclerView} 设置边距
+     * @param context         使用适配的上下文
+     * @param viewWidth       RecyclerView实际宽度，一般情况下就是屏幕宽度，但是如果有其他控件和它在同一行，需要计算实际宽度，不然无法正确显示RecyclerView item的布局
+     * @param imageCountLimit 最多显示的图片数目
+     * @param spanCount       每行显示的图片数目
      */
-    public EditableImageAdapter(Context context, int imageCountLimit, int spacing) {
+    public EditableImageAdapter(Context context, int viewWidth, int imageCountLimit, int spanCount) {
         this.context = context;
+        this.viewWidth = viewWidth;
         this.imageCountLimit = imageCountLimit;
-        this.spacing = spacing;
-        this.screenWidth = ContextHub.getScreenWidth(context);
+        this.spanCount = spanCount;
         this.layoutInflater = LayoutInflater.from(context);
     }
 
@@ -70,7 +70,10 @@ public class EditableImageAdapter extends RecyclerView.Adapter<ViewHolder> {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
         ImageView imageView = holder.getView(R.id.imageView);
-        configImageParams(imageView, position);
+        int imageSize = viewWidth / spanCount;
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(imageSize, imageSize);
+        imageView.setLayoutParams(params);
+
         if (position == getItemCount() - 1 && images.size() < imageCountLimit) {
             imageView.setImageResource(R.drawable.ic_add_pic);
             imageView.setOnClickListener(new View.OnClickListener() {
@@ -108,43 +111,6 @@ public class EditableImageAdapter extends RecyclerView.Adapter<ViewHolder> {
                 }
             });
         }
-    }
-
-    private void configImageParams(ImageView imageView, int position) {
-        int temp = IntHub.dp2px(context, spacing);
-        int imageSize = (screenWidth - temp * 3) / 3;
-
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(imageSize, imageSize);
-        switch (position) {
-            case 0:
-                params.setMargins(temp, temp, temp >> 1, temp >> 1);
-                break;
-            case 1:
-                params.setMargins(temp >> 1, temp, temp >> 1, temp >> 1);
-                break;
-            case 2:
-                params.setMargins(temp >> 1, temp, temp, temp >> 1);
-                break;
-            case 3:
-                params.setMargins(temp, temp >> 1, temp >> 1, temp >> 1);
-                break;
-            case 4:
-                params.setMargins(temp >> 1, temp >> 1, temp >> 1, temp >> 1);
-                break;
-            case 5:
-                params.setMargins(temp >> 1, temp >> 1, temp, temp >> 1);
-                break;
-            case 6:
-                params.setMargins(temp, temp >> 1, temp >> 1, temp);
-                break;
-            case 7:
-                params.setMargins(temp >> 1, temp >> 1, temp >> 1, temp);
-                break;
-            case 8:
-                params.setMargins(temp >> 1, temp >> 1, temp, temp);
-                break;
-        }
-        imageView.setLayoutParams(params);
     }
 
     @Override
