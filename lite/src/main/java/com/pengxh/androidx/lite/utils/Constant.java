a3d0f842eddcdbd0e0cb2be35a68a53a982ad934
pengxianhong <290677893@qq.com>
2024-05-28 14:09:35 +0800

    去掉BLE蓝牙封装

--- lite/src/main/java/com/pengxh/androidx/lite/utils/Constant.java
+++ lite/src/main/java/com/pengxh/androidx/lite/utils/Constant.java
@@ -33,24 +33,6 @@ public class Constant {
     public static final long HTTP_TIMEOUT = 15;
 
     /**
-     * 连接超时时间10s
-     */
-    public static final long MAX_CONNECT_TIME = 10000L;
-
-    public static final String BLUETOOTH_STATE_CHANGED = "android.bluetooth.adapter.action.STATE_CHANGED";
-    public static final int BLUETOOTH_ON = 20;
-    public static final int BLUETOOTH_OFF = 21;
-    public static final int CONNECT_SUCCESS = 22;
-    public static final int CONNECT_FAILURE = 23;
-    public static final int DISCONNECT_SUCCESS = 24;
-    public static final int SEND_SUCCESS = 25;
-    public static final int SEND_FAILURE = 26;
-    public static final int RECEIVE_SUCCESS = 27;
-    public static final int RECEIVE_FAILURE = 28;
-    public static final int DISCOVERY_DEVICE = 29;
-    public static final int DISCOVERY_OUT_TIME = 30;
-
-    /**
      * 录音动画图标
      */
     public static List<Integer> AUDIO_DRAWABLES = Arrays.asList(
