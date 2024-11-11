6de71601ee1d7468d3df5e8eb74a3c66ce1807c7
pengxianhong <290677893@qq.com>
2024-11-11 14:55:23 +0800

    优化byte[]转ASCII码代码

--- lite/src/main/java/com/pengxh/androidx/lite/kit/ByteArrayKit.java
+++ lite/src/main/java/com/pengxh/androidx/lite/kit/ByteArrayKit.java
@@ -2,15 +2,10 @@ package com.pengxh.androidx.lite.kit;
 
 public class ByteArrayKit {
     public static String toAsciiCode(byte[] bytes) {
-        //判断是否能被2整除
-        if (bytes.length % 2 == 0) {
-            StringBuilder builder = new StringBuilder();
-            for (byte aByte : bytes) {
-                builder.append((char) aByte);
-            }
-            return builder.toString();
-        } else {
-            return "Decode Error";
+        StringBuilder builder = new StringBuilder();
+        for (byte aByte : bytes) {
+            builder.append((char) aByte);
         }
+        return builder.toString();
     }
 }
