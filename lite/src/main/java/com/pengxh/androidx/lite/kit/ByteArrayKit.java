a277f2f17aa8baab48feb67a4a98dbfb3a7d2ded
pengxianhong <290677893@qq.com>
2024-11-11 15:02:16 +0800

    添加byte[]转Hex代码

--- lite/src/main/java/com/pengxh/androidx/lite/kit/ByteArrayKit.java
+++ lite/src/main/java/com/pengxh/androidx/lite/kit/ByteArrayKit.java
@@ -1,6 +1,17 @@
 package com.pengxh.androidx.lite.kit;
 
 public class ByteArrayKit {
+    public static String toHex(byte[] bytes) {
+        char[] hexArray = "0123456789ABCDEF".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int i = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[i >>> 4];
+            hexChars[j * 2 + 1] = hexArray[i & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
     public static String toAsciiCode(byte[] bytes) {
         StringBuilder builder = new StringBuilder();
         for (byte aByte : bytes) {
