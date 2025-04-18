dc5599589816b3e0cd6dac9c924382bd8c95a1af
pengxianhong <290677893@qq.com>
2025-04-18 15:24:39 +0800

    feat(widget): 为 EmptyView 添加可配置的重新加载按钮
    - 在 attrs.xml 中添加 EmptyView 的自定义属性 show_reload_button
    - 修改 EmptyView 构造函数，根据属性决定是否显示重新加载按钮
    - 删除未使用的 GlideLoadEngine 类

--- lite/src/main/java/com/pengxh/androidx/lite/widget/EmptyView.java
+++ lite/src/main/java/com/pengxh/androidx/lite/widget/EmptyView.java
@@ -1,11 +1,13 @@
 package com.pengxh.androidx.lite.widget;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import com.pengxh.androidx.lite.R;
 import com.pengxh.androidx.lite.databinding.WidgetViewEmptyBinding;
 
 public class EmptyView extends LinearLayout {
@@ -13,14 +15,22 @@ public class EmptyView extends LinearLayout {
     public EmptyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         WidgetViewEmptyBinding binding = WidgetViewEmptyBinding.inflate(LayoutInflater.from(context), this, true);
-        binding.reloadButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (listener != null) {
-                    listener.onReloadButtonClicked();
+        TypedArray type = context.obtainStyledAttributes(attrs, R.styleable.EmptyView);
+        boolean isShowReloadButton = type.getBoolean(R.styleable.EmptyView_show_reload_button, true);
+        type.recycle();
+        if (isShowReloadButton) {
+            binding.reloadButton.setVisibility(VISIBLE);
+            binding.reloadButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        listener.onReloadButtonClicked();
+                    }
                 }
-            }
-        });
+            });
+        } else {
+            binding.reloadButton.setVisibility(GONE);
+        }
     }
 
     private OnClickListener listener;
