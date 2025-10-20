bca4d64485c9e1bbcb5e56be7d743a4a9ba1d752
pengxianhong <290677893@qq.com>
2025-10-20 09:57:59 +0800

    refactor(utils):重构网络请求状态枚举类包路径- 将 HttpRequestState 枚举类从 utils 包移动到 enums 包 - 在 HttpRequestCallback 接口中引入新的枚举包路径依赖 - 更新了相关类的导入语句以匹配新的包结构

--- lite/src/main/java/com/pengxh/androidx/lite/enums/HttpRequestState.java
+++ lite/src/main/java/com/pengxh/androidx/lite/enums/HttpRequestState.java
@@ -1,4 +1,4 @@
-package com.pengxh.androidx.lite.utils;
+package com.pengxh.androidx.lite.enums;
 
 /**
  * 网络请求状态枚举类（用于不关注返回值，只关注请求状态的情况）
--- lite/src/main/java/com/pengxh/androidx/lite/utils/HttpRequestCallback.java
+++ lite/src/main/java/com/pengxh/androidx/lite/utils/HttpRequestCallback.java
@@ -1,5 +1,7 @@
 package com.pengxh.androidx.lite.utils;
 
+import com.pengxh.androidx.lite.enums.HttpRequestState;
+
 public interface HttpRequestCallback {
     void onStateChanged(HttpRequestState state);
 }
